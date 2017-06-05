b33f54ab2f69ed9724bc18a0608b5d05eaa518ed
Kamesh Balasubramanian <kamesh@wirecog.com>
2017-06-04 17:01:57 -0700

    Form validation example.

--- gopherfaceform/handlers/signup.go
+++ gopherfaceform/handlers/signup.go
@@ -1,6 +1,7 @@
 package handlers
 
 import (
+	"gocodelab/gopherfaceform/validationkit"
 	"net/http"
 )
 
@@ -13,6 +14,10 @@ type SignUpForm struct {
 // DisplaySignUpForm displays the Sign Up form
 func DisplaySignUpForm(w http.ResponseWriter, r *http.Request, s *SignUpForm) {
 	RenderTemplate(w, "./templates/signup.html", s)
+}
+
+func DisplayConfirmation(w http.ResponseWriter, r *http.Request, s *SignUpForm) {
+	RenderUnsafeTemplate(w, "./templates/signupconfirmation.html", s)
 }
 
 func PopulateFormFields(r *http.Request, s *SignUpForm) {
@@ -28,23 +33,79 @@ func ValidateSignUpForm(w http.ResponseWriter, r *http.Request, s *SignUpForm) {
 
 	PopulateFormFields(r, s)
 	// Check if username was filled out
+	if r.FormValue("username") == "" {
+		s.Errors["usernameError"] = "The username field is required."
+	}
 
 	// Check if first name was filled out
+	if r.FormValue("firstName") == "" {
+		s.Errors["firstNameError"] = "The first name field is required."
+	}
 
 	// Check if last name was filled out
+	if r.FormValue("lastName") == "" {
+		s.Errors["lastNameError"] = "The last name field is required."
+	}
 
 	// Check if e-mail address was filled out
+	if r.FormValue("email") == "" {
+		s.Errors["emailError"] = "The e-mail address field is required."
+	}
+
+	// Check if e-mail address was filled out
+	if r.FormValue("password") == "" {
+		s.Errors["passwordError"] = "The password field is required."
+	}
+
+	// Check if e-mail address was filled out
+	if r.FormValue("confirmPassword") == "" {
+		s.Errors["confirmPasswordError"] = "The confirm password field is required."
+	}
 
 	// Check username syntax
+	if validationkit.CheckUsernameSyntax(r.FormValue("username")) == false {
+
+		usernameErrorMessage := "The username field has an improper username syntax."
+		if _, ok := s.Errors["usernameError"]; ok {
+			s.Errors["username"] += " " + usernameErrorMessage
+		} else {
+			s.Errors["username"] = usernameErrorMessage
+		}
+	}
 
 	// Check e-mail address syntax
+	if validationkit.CheckEmailSyntax(r.FormValue("email")) == false {
+
+		emailErrorMessage := "The username field has an improper username syntax."
+		if _, ok := s.Errors["usernameError"]; ok {
+			s.Errors["email"] += " " + emailErrorMessage
+		} else {
+			s.Errors["email"] = emailErrorMessage
+		}
+	}
 
 	// Check if passord and confirm password field values match
+	if r.FormValue("password") != r.FormValue("confirmPassword") {
+		s.Errors["confirmPasswordError"] = "The password and confirm pasword fields do not match."
+	}
+
+	if len(s.Errors) > 0 {
+		DisplaySignUpForm(w, r, s)
+	} else {
+		ProcessSignUpForm(w, r, s)
+	}
 
 }
 
 // ProcessSignUpForm
-func ProcessSignUpForm(w http.ResponseWriter, r *http.Request) {
+func ProcessSignUpForm(w http.ResponseWriter, r *http.Request, s *SignUpForm) {
+
+	// If we reached this point, that indicates that we had a successful form submission.
+	// Later, we will include form processing logic here, in this case that would be
+	// inserting the information from the form as an entry into the database.
+
+	// Display form confirmation message
+	DisplayConfirmation(w, r, s)
 
 }
 
@@ -61,7 +122,7 @@ func SignUpHandler(w http.ResponseWriter, r *http.Request) {
 	case "GET":
 		DisplaySignUpForm(w, r, &s)
 	case "POST":
-		ValidateSignUpForm(w, r)
+		ValidateSignUpForm(w, r, &s)
 	default:
 		DisplaySignUpForm(w, r, &s)
 	}
--- gopherfaceform/handlers/utility.go
+++ gopherfaceform/handlers/utility.go
@@ -4,6 +4,7 @@ import (
 	"html/template"
 	"log"
 	"net/http"
+	ttemplate "text/template"
 )
 
 // Template rendering function
@@ -12,5 +13,14 @@ func RenderTemplate(w http.ResponseWriter, templateFile string, templateData int
 	if err != nil {
 		log.Fatal("Error encountered while parsing the template: ", err)
 	}
+	t.Execute(w, templateData)
+}
+
+func RenderUnsafeTemplate(w http.ResponseWriter, templateFile string, templateData interface{}) {
+	t, err := ttemplate.ParseFiles(templateFile)
+	if err != nil {
+		log.Fatal("Error encountered while parsing the template: ", err)
+	}
+	w.Header().Set("X-XSS-Protection", "0")
 	t.Execute(w, templateData)
 }
